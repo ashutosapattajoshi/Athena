4d5962545ce30220bfde9eac00fb30ef43683455
ashutosapattajoshi <ashutosapattajoshi@localhost>
2025-05-18 17:10:17 +0530

    git commit -m "updated logic for headless testing"

--- src/main/java/base/DriverFactory.java
+++ src/main/java/base/DriverFactory.java
@@ -18,11 +18,21 @@ public class DriverFactory {
     public static WebDriver initDriver() {
         String browser = prop.getProperty("browser");
         boolean isHeadless = Boolean.parseBoolean(prop.getProperty("headless"));
+        
+
+        System.out.println("Browser: " + browser);
+        System.out.println("Headless mode: " + isHeadless);
 
         if (browser.equalsIgnoreCase("chrome")) {
             WebDriverManager.chromedriver().setup();
             ChromeOptions options = new ChromeOptions();
-            if (isHeadless) options.addArguments("--headless=new");
+            if (isHeadless) {
+                options.addArguments("--headless=new");
+                options.addArguments("--no-sandbox");
+                options.addArguments("--disable-dev-shm-usage");
+                options.addArguments("--disable-gpu");
+                options.addArguments("--window-size=1920,1080");
+            }
             driver = new ChromeDriver(options);
         } else if (browser.equalsIgnoreCase("firefox")) {
             WebDriverManager.firefoxdriver().setup();
